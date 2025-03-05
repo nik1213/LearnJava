f121419f6a646d20f245ab5a56c327c1f217b1d3
nikhil ranjan <55246009+nik1213@users.noreply.github.com>
2025-03-06 02:44:07 +0530

    Update D5_Exercise2.java
    adding expected output in comment section

--- D5_Exercise2.java
+++ D5_Exercise2.java
@@ -2,7 +2,7 @@ public class D5_Exercise2 {
     public static void main(String[] args) {
         // write a java program to encrype a grade by adding 8 to it. Decript it to show correct grade.
         char grade = 'A';
-        grade = (char)(grade+8); // we have typecasted because we are adding char and integer 
+        grade = (char)(grade+8); // we have typecasted because we are adding char and integer. it will print I on place of A
         System.out.println(grade);
         //To decript this 
         char f = grade;
